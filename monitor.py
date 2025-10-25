cb320ce8a57a23e51f29412ad0b315e51961cbd5
psm5556 <psm5556@gmail.com>
2025-10-25 15:44:08 +0900

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -10,20 +10,22 @@ BOT_TOKEN = os.environ.get('BOT_TOKEN')
 CHAT_ID = os.environ.get('CHAT_ID')
 
 TICKERS = [
-    "AAPL", "ABB", "ABCL", "ACHR", "AEP",
-    "AES", "ALAB", "AMD", "AMZN", "ANET", "ARQQ", "ARRY", "ASML", "ASTS", "AVGO",
-    "BA", "BAC", "BE", "BEP", "BLK", "BMNR", "BP", "BTQ", "BWXT", "C", "CARR",
-    "CDNS", "CEG", "CFR.SW", "CGON", "CLPT", "COIN", "CONE", "CONL", "COP", "COST",
-    "CRCL", "CRDO", "CRM", "CRSP", "CSCO", "CVX", "D", "DELL", "DNA", "DUK", "ED",
-    "EMR", "ENPH", "ENR", "EOSE", "EQIX", "ETN", "EXC", "FLNC", "FSLR", "GEV", "GLD",
-    "GOOGL", "GS", "HOOD", "HSBC", "HUBB", "IBM", "INTC", "IONQ", "JCI", "JOBY", "JPM",
-    "KO", "LAES", "LMT", "LRCX", "LVMUY", "MA", "MPC", "MSFT", "MSTR", "NEE", "NGG",
-    "NOC", "NRG", "NRGV", "NTLA", "NTRA", "NVDA", "OKLO", "ON", "ORCL", "OXY", "PCG",
-    "PG", "PLTR", "PLUG", "PSTG", "PYPL", "QBTS", "QS", "QUBT", "QURE", "RGTI", "RKLB",
-    "ROK", "SBGSY", "SEDG", "SHEL", "SIEGY", "SLDP", "SMR", "SNPS", "SO", "SOFI",
-    "SPCE", "SPWR", "SQ", "SRE", "STEM", "TLT", "TMO", "TSLA", "TSM", "TWST", "UBT",
-    "UNH", "V", "VLO", "VRT", "VST", "WMT", "HON", "TXG", "XOM", "ZPTA"
-]
+    "AAPL", "ABCL", "ACHR", "AEP", "AES", "ALAB", "AMD", "AMZN", "ANET",
+    "ARQQ", "ARRY", "ASML", "ASTS", "AVGO", "BA", "BAC", "BE", "BEP",
+    "BLK", "BMNR", "BP", "BTQ", "BWXT", "C", "CARR", "CDNS", "CEG",
+    "CFR.SW", "CGON", "CLPT", "COIN", "COP", "COST", "CRCL", "CRDO",
+    "CRM", "CRSP", "CSCO", "CVX", "D", "DELL", "DNA", "DUK", "ED", "EMR",
+    "ENPH", "ENR", "EOSE", "EQIX", "ETN", "EXC", "FLNC", "FSLR", "GEV",
+    "GLD", "GOOGL", "GS", "HOOD", "HSBC", "HUBB", "IBM", "INTC", "IONQ",
+    "JCI", "JOBY", "JPM", "KO", "LAES", "LMT", "LRCX", "LVMUY", "MA",
+    "MPC", "MSFT", "MSTR", "NEE", "NGG", "NOC", "NRG", "NRGV", "NTLA",
+    "NTRA", "NVDA", "OKLO", "ON", "ORCL", "OXY", "PCG", "PG", "PLTR",
+    "PLUG", "PSTG", "PYPL", "QBTS", "QS", "QUBT", "QURE", "RGTI",
+    "RKLB", "ROK", "SBGSY", "SEDG", "SHEL", "SIEGY", "SLDP", "SMR",
+    "SNPS", "SO", "SOFI", "SPCE", "SPWR", "SQ", "SRE", "STEM", "TLT",
+    "TMO", "TSLA", "TSM", "TWST", "UBT", "UNH", "V", "VLO", "VRT", "VST",
+    "WMT", "HON", "TXG", "XOM", "ZPTA"
+]  # ABB & CONE 제거됨
 
 MA_LIST = [200, 240, 365]
 
